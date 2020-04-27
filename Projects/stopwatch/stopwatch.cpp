393ed32887ff3c47a2795d983248b7670a0d5383
HumorLogic <humor.ylw@outlook.com>
2020-04-27 11:03:55 +0800

    update

---
+++ Projects/stopwatch/stopwatch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+
+
+// 把main_*() 改为main()再运行
+int main() {
+
+    using namespace std;
+
+    system("CLS");
+    int HH = 0, MM = 0, SS = 0;
+    cout << "\n\t\t stopwatch";
+    cout << "\n\t\t HH: MM: SS";
+    cout << "\n\t\t " << HH << " : " << MM << " : " << SS;
+    cout << "\n\t\t press any key to start";
+    _getch();
+    while (!_kbhit())
+    {
+        SS++;
+        Sleep(1000);
+        if (SS > 59)
+        {
+            MM++;
+            SS = 0;
+        }
+        if (MM > 59)
+        {
+            HH++;
+            MM = 0;
+        }
+        system("CLS");
+        cout << "\n\t\t stopwatch";
+        cout << "\n\t\t HH: MM: SS";
+        cout << "\n\t\t " << HH << " : " << MM << " : " << SS;
+        cout << "\n\t\t press any key to stop";
+    }
+    _getch();
+    cout << "\n\t\t the time after pausing is";
+    cout << "\n\t\t " << HH << " : " << MM << " : " << SS;
+    _getch();
+    return 0;
+
+
+
+}
